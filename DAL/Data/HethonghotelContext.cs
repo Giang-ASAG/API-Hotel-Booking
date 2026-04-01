8c6a283166fd88a74e59a1e6322471c441869f67
DESKTOP-N261OGO\anhru <anhrutro@gmail.com>
2026-04-01 13:48:11 +0700

    update project

---
+++ DAL/Data/HethonghotelContext.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using DH52110843_DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace DH52110843_DAL.Data;
+
+public partial class HethonghotelContext : DbContext
+{
+    public HethonghotelContext()
+    {
+    }
+
+    public HethonghotelContext(DbContextOptions<HethonghotelContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Bookingroom> Bookingrooms { get; set; }
+
+    public virtual DbSet<Hotel> Hotels { get; set; }
+
+    public virtual DbSet<HotelImage> HotelImages { get; set; }
+
+    public virtual DbSet<HotelImagesStorage> HotelImagesStorages { get; set; }
+
+    public virtual DbSet<HotelService> HotelServices { get; set; }
+
+    public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    public virtual DbSet<Room> Rooms { get; set; }
+
+    public virtual DbSet<RoomImage> RoomImages { get; set; }
+
+    public virtual DbSet<RoomImagesStorage> RoomImagesStorages { get; set; }
+
+    public virtual DbSet<RoomService> RoomServices { get; set; }
+
+    public virtual DbSet<RoomType> RoomTypes { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<UserDocument> UserDocuments { get; set; }
+
+    public virtual DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("Server=db26741.databaseasp.net; Database=db26741; Uid=db26741; Pwd=4Kn_Yo=8w2Z+;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.2.0-mysql"));
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseMySql("server=localhost;database=hethonghotel;user=root;password=123123;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.2.0-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.BookingId).HasName("PRIMARY");
+
+            entity.ToTable("bookings");
+
+            entity.HasIndex(e => e.UserId, "user_id");
+
+            entity.Property(e => e.BookingId).HasColumnName("booking_id");
+            entity.Property(e => e.CheckInDate)
+                .HasColumnType("timestamp")
+                .HasColumnName("check_in_date");
+            entity.Property(e => e.CheckOutDate)
+                .HasColumnType("timestamp")
+                .HasColumnName("check_out_date");
+            entity.Property(e => e.Status)
+                .HasDefaultValueSql("'0'")
+                .HasColumnName("status");
+            entity.Property(e => e.TotalAmount).HasColumnName("total_amount");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("bookings_ibfk_1");
+        });
+
+        modelBuilder.Entity<Bookingroom>(entity =>
+        {
+            entity.HasKey(e => e.BkroomsId).HasName("PRIMARY");
+
+            entity.ToTable("bookingroom");
+
+            entity.HasIndex(e => e.BookingId, "booking_id");
+
+            entity.HasIndex(e => e.RoomId, "room_id");
+
+            entity.Property(e => e.BkroomsId).HasColumnName("bkrooms_Id");
+            entity.Property(e => e.BookingId).HasColumnName("booking_id");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.RoomId).HasColumnName("room_id");
+
+            entity.HasOne(d => d.Booking).WithMany(p => p.Bookingrooms)
+                .HasForeignKey(d => d.BookingId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("bookingroom_ibfk_1");
+
+            entity.HasOne(d => d.Room).WithMany(p => p.Bookingrooms)
+                .HasForeignKey(d => d.RoomId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("bookingroom_ibfk_2");
+        });
+
+        modelBuilder.Entity<Hotel>(entity =>
+        {
+            entity.HasKey(e => e.HotelId).HasName("PRIMARY");
+
+            entity.ToTable("hotels");
+
+            entity.HasIndex(e => e.Email, "email").IsUnique();
+
+            entity.HasIndex(e => e.UserId, "user_id");
+
+            entity.Property(e => e.HotelId).HasColumnName("hotel_id");
+            entity.Property(e => e.Address)
+                .HasColumnType("text")
+                .HasColumnName("address");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.HotelName)
+                .HasMaxLength(100)
+                .HasColumnName("hotel_name");
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .HasColumnName("phone_number");
+            entity.Property(e => e.Star).HasPrecision(5, 2);
+            entity.Property(e => e.Status).HasColumnName("status");
+            entity.Property(e => e.UpdatedAt)
+                .ValueGeneratedOnAddOrUpdate()
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.XCoordinate).HasColumnName("x_coordinate");
+            entity.Property(e => e.YCoordinate).HasColumnName("y_coordinate");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Hotels)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("hotels_ibfk_1");
+        });
+
+        modelBuilder.Entity<HotelImage>(entity =>
+        {
+            entity.HasKey(e => e.HotelImageId).HasName("PRIMARY");
+
+            entity.ToTable("hotel_images");
+
+            entity.HasIndex(e => e.HotelId, "hotel_id");
+
+            entity.HasIndex(e => e.ImageId, "image_id");
+
+            entity.Property(e => e.HotelImageId).HasColumnName("hotel_image_id");
+            entity.Property(e => e.HotelId).HasColumnName("hotel_id");
+            entity.Property(e => e.ImageId).HasColumnName("image_id");
+
+            entity.HasOne(d => d.Hotel).WithMany(p => p.HotelImages)
+                .HasForeignKey(d => d.HotelId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("hotel_images_ibfk_1");
+
+            entity.HasOne(d => d.Image).WithMany(p => p.HotelImages)
+                .HasForeignKey(d => d.ImageId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("hotel_images_ibfk_2");
+        });
+
+        modelBuilder.Entity<HotelImagesStorage>(entity =>
+        {
+            entity.HasKey(e => e.ImageId).HasName("PRIMARY");
+
+            entity.ToTable("hotel_images_storage");
+
+            entity.Property(e => e.ImageId).HasColumnName("image_id");
+            entity.Property(e => e.ImagePath)
+                .HasMaxLength(255)
+                .HasColumnName("image_path");
+        });
+
+        modelBuilder.Entity<HotelService>(entity =>
+        {
+            entity.HasKey(e => e.ServiceId).HasName("PRIMARY");
+
+            entity.ToTable("hotel_services");
+
+            entity.HasIndex(e => e.HotelId, "hotel_id");
+
+            entity.Property(e => e.ServiceId).HasColumnName("service_id");
+            entity.Property(e => e.HotelId).HasColumnName("hotel_id");
+            entity.Property(e => e.ServiceInfo)
+                .HasColumnType("text")
+                .HasColumnName("service_info");
+            entity.Property(e => e.ServiceName)
+                .HasMaxLength(100)
+                .HasColumnName("service_name");
+
+            entity.HasOne(d => d.Hotel).WithMany(p => p.HotelServices)
+                .HasForeignKey(d => d.HotelId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("hotel_services_ibfk_1");
+        });
+
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(e => e.PaymentId).HasName("PRIMARY");
+
+            entity.ToTable("payments");
+
+            entity.HasIndex(e => e.BookingId, "booking_id");
+
+            entity.Property(e => e.PaymentId).HasColumnName("payment_id");
+            entity.Property(e => e.BookingId).HasColumnName("booking_id");
+            entity.Property(e => e.Note)
+                .HasMaxLength(255)
+                .HasColumnName("note");
+            entity.Property(e => e.PaymentDate)
+                .HasColumnType("timestamp")
+                .HasColumnName("payment_date");
+            entity.Property(e => e.PaymentMethod)
+                .HasMaxLength(100)
+                .HasColumnName("payment_method");
+            entity.Property(e => e.TotalAmount).HasColumnName("total_amount");
+
+            entity.HasOne(d => d.Booking).WithMany(p => p.Payments)
+                .HasForeignKey(d => d.BookingId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("payments_ibfk_1");
+        });
+
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.HasKey(e => e.ReviewId).HasName("PRIMARY");
+
+            entity.ToTable("reviews");
+
+            entity.HasIndex(e => e.HotelId, "hotel_id");
+
+            entity.HasIndex(e => e.UserId, "user_id");
+
+            entity.Property(e => e.ReviewId).HasColumnName("review_id");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.HotelId).HasColumnName("hotel_id");
+            entity.Property(e => e.StarRating).HasColumnName("star_rating");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Hotel).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.HotelId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("reviews_ibfk_2");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("reviews_ibfk_1");
+        });
+
+        modelBuilder.Entity<Room>(entity =>
+        {
+            entity.HasKey(e => e.RoomId).HasName("PRIMARY");
+
+            entity.ToTable("rooms");
+
+            entity.HasIndex(e => e.RoomTypeId, "rooms_ibfk_2");
+
+            entity.Property(e => e.RoomId).HasColumnName("room_id");
+            entity.Property(e => e.Active).HasColumnName("active");
+            entity.Property(e => e.RoomNumber)
+                .HasMaxLength(10)
+                .HasColumnName("room_number");
+            entity.Property(e => e.RoomTypeId).HasColumnName("room_type_id");
+            entity.Property(e => e.Status).HasColumnName("status");
+
+            entity.HasOne(d => d.RoomType).WithMany(p => p.Rooms)
+                .HasForeignKey(d => d.RoomTypeId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("rooms_ibfk_2");
+        });
+
+        modelBuilder.Entity<RoomImage>(entity =>
+        {
+            entity.HasKey(e => e.RoomImageId).HasName("PRIMARY");
+
+            entity.ToTable("room_images");
+
+            entity.HasIndex(e => e.ImageId, "image_id");
+
+            entity.HasIndex(e => e.RoomTypeId, "room_type_id");
+
+            entity.Property(e => e.RoomImageId).HasColumnName("room_image_id");
+            entity.Property(e => e.ImageId).HasColumnName("image_id");
+            entity.Property(e => e.RoomTypeId).HasColumnName("room_type_id");
+
+            entity.HasOne(d => d.Image).WithMany(p => p.RoomImages)
+                .HasForeignKey(d => d.ImageId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("room_images_ibfk_2");
+
+            entity.HasOne(d => d.RoomType).WithMany(p => p.RoomImages)
+                .HasForeignKey(d => d.RoomTypeId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("room_images_ibfk_1");
+        });
+
+        modelBuilder.Entity<RoomImagesStorage>(entity =>
+        {
+            entity.HasKey(e => e.ImageId).HasName("PRIMARY");
+
+            entity.ToTable("room_images_storage");
+
+            entity.Property(e => e.ImageId).HasColumnName("image_id");
+            entity.Property(e => e.ImagePath)
+                .HasMaxLength(255)
+                .HasColumnName("image_path");
+        });
+
+        modelBuilder.Entity<RoomService>(entity =>
+        {
+            entity.HasKey(e => e.ServiceId).HasName("PRIMARY");
+
+            entity.ToTable("room_services");
+
+            entity.HasIndex(e => e.RoomTypeId, "room_type_id");
+
+            entity.Property(e => e.ServiceId).HasColumnName("service_id");
+            entity.Property(e => e.RoomTypeId).HasColumnName("room_type_id");
+            entity.Property(e => e.ServiceName)
+                .HasMaxLength(100)
+                .HasColumnName("service_name");
+
+            entity.HasOne(d => d.RoomType).WithMany(p => p.RoomServices)
+                .HasForeignKey(d => d.RoomTypeId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("room_services_ibfk_1");
+        });
+
+        modelBuilder.Entity<RoomType>(entity =>
+        {
+            entity.HasKey(e => e.RoomTypeId).HasName("PRIMARY");
+
+            entity.ToTable("room_types");
+
+            entity.HasIndex(e => e.HotelId, "room_types_ibfk_hotel");
+
+            entity.Property(e => e.RoomTypeId).HasColumnName("room_type_id");
+            entity.Property(e => e.Capacity).HasColumnName("capacity");
+            entity.Property(e => e.HotelId).HasColumnName("hotel_id");
+            entity.Property(e => e.Price).HasColumnName("price");
+            entity.Property(e => e.RoomInfo)
+                .HasColumnType("text")
+                .HasColumnName("room_info");
+            entity.Property(e => e.TypeName)
+                .HasMaxLength(100)
+                .HasColumnName("type_name");
+
+            entity.HasOne(d => d.Hotel).WithMany(p => p.RoomTypes)
+                .HasForeignKey(d => d.HotelId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("room_types_ibfk_hotel");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "email").IsUnique();
+
+            entity.HasIndex(e => e.UserRoleId, "user_role_id");
+
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.Active)
+                .HasDefaultValueSql("'0'")
+                .HasColumnName("active");
+            entity.Property(e => e.Address)
+                .HasColumnType("text")
+                .HasColumnName("address");
+            entity.Property(e => e.CreationDate).HasColumnName("creation_date");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.FullName)
+                .HasMaxLength(100)
+                .HasColumnName("full_name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .HasColumnName("password");
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .HasColumnName("phone_number");
+            entity.Property(e => e.UserRoleId).HasColumnName("user_role_id");
+
+            entity.HasOne(d => d.UserRole).WithMany(p => p.Users)
+                .HasForeignKey(d => d.UserRoleId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("users_ibfk_1");
+        });
+
+        modelBuilder.Entity<UserDocument>(entity =>
+        {
+            entity.HasKey(e => e.IdDocument).HasName("PRIMARY");
+
+            entity.ToTable("user_document");
+
+            entity.HasIndex(e => e.UserId, "FK_UserDocument_User");
+
+            entity.Property(e => e.IdDocument).HasColumnName("id_document");
+            entity.Property(e => e.Active).HasColumnName("active");
+            entity.Property(e => e.BankAccountNumber).HasMaxLength(50);
+            entity.Property(e => e.BankName).HasMaxLength(100);
+            entity.Property(e => e.CccdNumber).HasMaxLength(50);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime");
+            entity.Property(e => e.TaxCode).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<UserRole>(entity =>
+        {
+            entity.HasKey(e => e.UserRoleId).HasName("PRIMARY");
+
+            entity.ToTable("user_roles");
+
+            entity.Property(e => e.UserRoleId).HasColumnName("user_role_id");
+            entity.Property(e => e.RoleName)
+                .HasMaxLength(50)
+                .HasColumnName("role_name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DAL/Repository/UserRepository.cs
@@ -0,0 +1,129 @@
+﻿using DH52110843_DAL.Data;
+using DH52110843_DAL.Interfaces;
+using DH52110843_DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH52110843_DAL.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly HethonghotelContext db;
+        public UserRepository(HethonghotelContext db)
+        {
+            this.db = db;
+        }
+        public async Task AddAsync(User user)
+        {
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await db.Users.FindAsync(id);
+            if(user != null)
+            {
+                db.Users.Remove(user);
+                await db.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Not found User with ID {id}");
+            }
+
+        }
+
+        public async Task<User> findUserbyId(int id)
+        {
+            var user = await db.Users.FindAsync(id);
+            if(user == null)
+            {
+                throw new KeyNotFoundException($"Not found User with ID {id}");
+            }
+            else
+            {
+                return user;
+            }
+
+        }
+        public async Task<User> FindUserByEmailAsync(string email)
+        {
+            var user = await db.Users.FirstOrDefaultAsync(x => x.Email == email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> getAllAsync()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        public async Task<int[]> GetUserCountsByMonth()
+        {
+            int[] counts = new int[12];
+            var users = await db.Users.Select(x=>x.CreationDate).ToListAsync();
+
+            foreach (var item in users)
+            {
+                int month = item.Value.Month;
+                counts[month - 1]++;
+            }
+            return counts;
+        }
+
+        public async Task UpdateAsync(int id,User user)
+        {
+            var u = await db.Users.FindAsync(id);
+            if(u != null)
+            {
+                
+                u.FullName = user.FullName;
+                u.Address = user.Address;
+                u.PhoneNumber = user.PhoneNumber;
+                if (user.UserRoleId > 0)
+                {
+                    u.UserRoleId = user.UserRoleId;
+                }
+                u.Active = user.Active;
+                await db.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Not found User with ID {id}");
+            }
+        }
+        public async Task UpdatePermissionAsync(int id)
+        {
+            var u = await db.Users.FirstOrDefaultAsync(x=>x.UserId== id);
+            if (u != null)
+            {
+                u.UserRoleId = 3;
+                await db.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Not found User with ID {id}");
+            }
+        }
+
+        public async Task ActiveAsync(int id, bool active)
+        {
+            var u = await db.Users.FindAsync(id);
+            if (u == null)
+            {
+                throw new KeyNotFoundException($"Not found User with ID {id}");
+            }
+            u.Active = active; await db.SaveChangesAsync();
+        }
+    }
+}
